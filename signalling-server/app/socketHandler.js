a2c82b0cb79b34b281d2f4958121acb57119be50
Gyeony <Gyeony@testui-MacBook-Pro.local>
2020-08-11 19:10:47 +0900

    add sigmalling server

---
+++ signalling-server/app/socketHandler.js
@@ -0,0 +1,45 @@
+module.exports = function(io, streams) {
+
+  io.on('connection', function(client) {
+    console.log('-- ' + client.id + ' joined --');
+    client.emit('id', client.id);
+
+    client.on('message', function (details) {
+      var otherClient = io.sockets.connected[details.to];
+
+      if (!otherClient) {
+        return;
+      }
+        delete details.to;
+        details.from = client.id;
+        otherClient.emit('message', details);
+    });
+
+
+    // client.on('exchange', function (localid) {
+    //   console.log('테스트 ');
+    //   console.log(' 아이디 : '+ localid);
+    //   client.emit('exchange',localid);
+    // });
+      
+    client.on('readyToStream', function(options) {
+      console.log('-- ' + client.id + ' is ready to stream --');
+      //고유아이디랑 이름을 스트림을 넘김
+      streams.addStream(client.id, options.name, options.connect); 
+     
+    });
+    
+    client.on('update', function(options) {
+      streams.update(client.id, options.name, true);
+      console.log('업뎃 실행'+options.name);
+    });
+
+    function leave() {
+      console.log('-- ' + client.id + ' left --');
+      streams.removeStream(client.id);
+    }
+
+    client.on('disconnect', leave);
+    client.on('leave', leave);
+  });
+};
---
+++ signalling-server/app/streams.js
@@ -0,0 +1,49 @@
+module.exports = function() {
+  /**
+   * available streams 
+   * the id value is considered unique (provided by socket.io)
+   */
+  var streamList = [];
+
+  /**
+   * Stream object
+   */
+  //아이디와 이름을 전달받음
+  var Stream = function(id, name, connect) {
+    this.name = name;
+    this.id = id;
+    this.connect = connect;
+  }
+
+  return {
+    //스트림 추가할때 //소켓핸들러에서 추가해줌
+    addStream : function(id, name, connect) {
+      var stream = new Stream(id, name, connect);
+     
+      //스트림 리스트에 추가
+      streamList.push(stream);
+    },
+
+    removeStream : function(id) {
+      var index = 0;
+      while(index < streamList.length && streamList[index].id != id){
+        index++;
+      }
+      streamList.splice(index, 1);
+    },
+
+    // update function
+    update : function(id, name ,connect) {
+      var stream = streamList.find(function(element, i, array) {
+        return element.id == id;
+      });
+      stream.name = name;
+      stream.connect = true;
+      console.log('업뎃 실행22'+name);
+    },
+
+    getStreams : function() {
+      return streamList;
+    }
+  }
+};
